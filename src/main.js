e10eee520af01f9ad96f3c7da18660a1cb22b8ad
caleb-okkers <okkerscaleb@gmail.com>
2024-06-26 09:10:01 +0200

    :wheelchair:

--- src/main.js
+++ src/main.js
@@ -3,5 +3,7 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import 'bootstrap/dist/css/boostrap.min.css'
+import bootstrap from
 
 createApp(App).use(store).use(router).mount('#app')
